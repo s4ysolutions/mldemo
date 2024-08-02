32efe1133f3d428a20d83d390ea7c0d0b7db02a6
dsa <sergey@s4y.solutions>
2024-08-03 00:32:43 +0200

    Reflect package rename

--- voice-transcription/src/main/java/solutions/s4y/mldemo/voice_transcription/ui/VoiceTranscriptionScreen.kt
+++ voice-transcription/src/main/java/solutions/s4y/mldemo/voice_transcription/ui/VoiceTranscriptionScreen.kt
@@ -1,4 +1,4 @@
-package solutions.s4y.mldemo.voice_transcription
+package solutions.s4y.mldemo.voice_transcription.ui
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
