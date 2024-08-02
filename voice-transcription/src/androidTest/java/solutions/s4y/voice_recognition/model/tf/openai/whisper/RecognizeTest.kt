0921a6b9763ba9a7669a85a1995be5a217b09b32
dsa <sergey@s4y.solutions>
2024-08-03 00:31:47 +0200

    Depricate Wav2vec2

--- voice-transcription/src/androidTest/java/solutions/s4y/voice_recognition/model/tf/openai/whisper/RecognizeTest.kt
+++ voice-transcription/src/androidTest/java/solutions/s4y/voice_recognition/model/tf/openai/whisper/RecognizeTest.kt
@@ -1,5 +1,6 @@
 package solutions.s4y.voice_recognition.model.tf.openai.whisper
 
+/*
 import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -8,7 +9,6 @@ import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import solutions.s4y.mel.KotlinMelSpectrogramProvider
 import solutions.s4y.pcm.PCMAssetWavProvider
-import solutions.s4y.voice_recognition.tf.model.whisper.WhisperVoiceRecognizer
 
 @RunWith(Enclosed::class)
 class RecognizeTest {
@@ -71,3 +71,4 @@ class RecognizeTest {
         }
     }
 }
+ */
